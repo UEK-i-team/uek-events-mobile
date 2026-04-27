5aed6f134d479f3aa2e0199d59f4d74a354a1309
NikolaDeja <nikolaa.deja@gmail.com>
2026-04-27 19:19:57 +0200

    widok wydarzenia ktore sie odbylo
    rebase but broken
    
    widok wydarzenia ktore sie odbylo

--- features/home/components/event-card/event-card.styles.tsx
+++ features/home/components/event-card/event-card.styles.tsx
@@ -11,6 +11,7 @@ export const styles = StyleSheet.create({
     borderRadius: 16,
     overflow: "hidden",
     flexDirection: "column",
+    paddingHorizontal: 16,
   },
   imageContainer: {
     width: "100%",
@@ -43,5 +44,56 @@ export const styles = StyleSheet.create({
   },
   infoContainer: {
     paddingHorizontal: 10,
+  },
+  // passedOverlay: {
+  //   position: "absolute",
+  //   top: 0,
+  //   left: 0,
+  //   right: 0,
+  //   bottom: 0,
+  //   backgroundColor: "#333",
+  //   justifyContent: "center",
+  //   alignItems: "center",
+  //   borderRadius: 30,
+  //   opacity: 0.6,
+  // },
+
+passedImage: {
+  opacity: 0.82,
+},
+
+passedOverlayGray: {
+  backgroundColor: "rgba(110, 110, 110, 0.5)",
+  position: "absolute",
+  top: 0,
+  left: 0,
+  right: 0,
+  bottom: 0,
+  borderRadius: 30,
+},
+
+passedOverlayDark: {
+  backgroundColor: "rgba(0, 0, 0, 0.3)",
+  position: "absolute",
+  top: 0,
+  left: 0,
+  right: 0,
+  bottom: 0,
+  borderRadius: 30,
+},
+  passedEventMarker: {
+    position: "absolute",
+    top: 10,
+    left: 10,
+    backgroundColor: "#424242",
+    paddingHorizontal: 20,
+    paddingVertical: 12,
+    borderRadius: 39,
+  },
+  passedText: {
+    color: "white",
+    fontSize: 14,
+    fontWeight: "300",
+    textAlign: "center",
   },
 });
--- features/home/components/event-card/event-card.tsx
+++ features/home/components/event-card/event-card.tsx
@@ -4,6 +4,7 @@ import { Image } from "expo-image";
 import { useRouter } from "expo-router";
 import React from "react";
 import {
+  Pressable,
   Text,
   TouchableOpacity,
   View,
@@ -11,6 +12,12 @@ import {
 } from "react-native";
 
 import { useResizeDominantBackgroundColor } from "../../hooks/use-resize-dominant-background-color";
+import { RoundedButton } from "@/shared/components/rounded-button/rounded-button";
+import { IEvent } from "@/shared/types/event";
+
+
+import { theme } from "@/shared/constants/theme";
+import { isEventPassed } from "@/utils/functions/date-utils";
 import { Badge } from "../badge/badge";
 import { DateAndTime } from "../date-and-time/date-and-time";
 import { Location } from "../location/location";
@@ -28,6 +35,7 @@ interface EventCardProps {
 }
 
 const TAG_COLORS = ["#B4DEFF", "#FAE5FF", "#C3F2EC"];
+const TAG_COLORS_PAST_EVENTS = ["#BDBDBD"];
 
 export const EventCard = React.memo(function EventCard({
   event,
@@ -69,6 +77,8 @@ export const EventCard = React.memo(function EventCard({
     toggleFavorite(event.id, !event.isFavorite);
   };
 
+
+  const eventHasPassed = isEventPassed(event.end_date);
   let imageHeight = 0;
 
   if(isVerySmallScreen){
@@ -79,6 +89,18 @@ export const EventCard = React.memo(function EventCard({
     imageHeight = Math.max(160, Math.min(220, cardHeight * 0.32));
   }
 
+
+
+  const eventHasPassed = isEventPassed(event.end_date);
+  let imageHeight = 0;
+
+  if(isVerySmallScreen){
+    imageHeight = 180;
+  } else if(cardHeight > 560){
+    imageHeight = Math.max(160, Math.min(220, cardHeight * 0.40));
+  } else {
+    imageHeight = Math.max(160, Math.min(220, cardHeight * 0.32));
+  }
 
   return (
     <View
@@ -96,8 +118,8 @@ export const EventCard = React.memo(function EventCard({
         style={[
           styles.card,
           {
-            width: SCREEN_WIDTH - 28,
-            height: cardHeight - 32,
+            width: SCREEN_WIDTH,
+            height: cardHeight,
           },
         ]}
       >
@@ -110,6 +132,34 @@ export const EventCard = React.memo(function EventCard({
             customWidth={SCREEN_WIDTH - 28}
             cornerRadius={16}
           />
+        <View
+          style={[styles.imageContainer, { height: imageHeight }]}
+        >
+          
+          <View>
+            <Image
+              source={{ uri: event.image_url }}
+              style={[styles.image, eventHasPassed && styles.passedImage]}
+              contentFit="cover"
+              cachePolicy="disk"
+              transition={200}
+            />
+            {eventHasPassed && (
+              // <View
+              //   style={styles.passedOverlay}
+              // />
+              <>
+                <View style={styles.passedOverlayGray} />
+                <View style={styles.passedOverlayDark} />
+              </>
+            )}
+          </View>
+          {eventHasPassed && (
+            <View style={styles.passedEventMarker}>
+              <Text style={styles.passedText}>Wydarzenie zakończone</Text>
+            </View>
+          )}
+        
           <RoundedButton
             icon={event.isFavorite ? FavoriteIconFilled : FavoriteIcon}
             iconColor={
@@ -169,8 +219,9 @@ export const EventCard = React.memo(function EventCard({
               <Badge
                 key={tag + index}
                 name={tag}
-                color={TAG_COLORS[index] || TAG_COLORS[0]}
+                color={eventHasPassed ? TAG_COLORS_PAST_EVENTS[0] : (TAG_COLORS[index % TAG_COLORS.length] || TAG_COLORS[0])}
                 textColor={theme.light.dark_grey}
+                
               />
             ))}
           </View>
