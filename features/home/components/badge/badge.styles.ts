5594c32ee93c916f5d0d2870c42adfa0e0c2261f
majasadowska <148617260+majasadowska@users.noreply.github.com>
2026-07-03 23:46:05 +0200

    Update badge styles with new border radius and width

--- features/home/components/badge/badge.styles.ts
+++ features/home/components/badge/badge.styles.ts
@@ -4,7 +4,9 @@ export const styles = StyleSheet.create({
   badge: {
     paddingHorizontal: 16,
     paddingVertical: 10,
-    borderRadius: 12,
+    borderRadius: 16,
+    borderWidth: 1,
+    bordeeColor: '#11111',
     alignSelf: "flex-start",
   },
   text: {
