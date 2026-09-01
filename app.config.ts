18c5d3c3c25eb5c71e98a28fa8ea5787db689e1c
szymonbrud <szymonbrud@MacBook-Pro-Szymon-2.local>
2026-09-01 17:50:02 +0200

    Update market version

--- app.config.ts
+++ app.config.ts
@@ -7,7 +7,7 @@ export default ({ config }: ConfigContext): ExpoConfig => {
     ...config,
     name: "Eventuje",
     slug: "uek-events-mobile",
-    version: "1.0.0",
+    version: "1.1.0",
     orientation: "portrait",
     icon: "./assets/images/icon.png",
     scheme: "uekeventsmobile",
