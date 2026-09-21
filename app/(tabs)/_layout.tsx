e7c80b379c37c5b94aa62de920808deba9a1c6c0
majasadowska <148617260+majasadowska@users.noreply.github.com>
2026-09-21 19:36:23 +0200

    Refactor TabLayout by removing FiltersTabButton
    Removed FiltersTabButton component and associated imports.

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,69 +1,17 @@
-import { BottomTabBarButtonProps } from "@react-navigation/bottom-tabs";
-import { PlatformPressable } from "@react-navigation/elements";
-import * as Haptics from "expo-haptics";
 import { Tabs } from "expo-router";
 import React from "react";
-import Animated, {
-  useAnimatedStyle,
-  useSharedValue,
-  withTiming,
-} from "react-native-reanimated";
 
-import { useFilters } from "@/features/filters/contexts/filters-context";
 import { HapticTab } from "@/shared/components/haptic-tab/haptic-tab";
 import { SvgIcon } from "@/shared/components/svg-icon/svg-icon";
 import { useTheme } from "@/shared/context/ThemeContext";
 
 // import icons
-import FilterIconOutline from "@/assets/icons/filter-icon-outline.svg";
 import HeartIconFilled from "@/assets/icons/heart-icon-filled.svg";
 import HeartIconOutline from "@/assets/icons/heart-icon-outline.svg";
 import HomeIconFilled from "@/assets/icons/home-icon-filled.svg";
 import HomeIconOutline from "@/assets/icons/home-icon-outline.svg";
 import InfoIconFilled from "@/assets/icons/info-icon-filled.svg";
 import InfoIconOutline from "@/assets/icons/info-icon-outline.svg";
-
-const AnimatedPressable = Animated.createAnimatedComponent(PlatformPressable);
-
-function FiltersTabButton(props: BottomTabBarButtonProps) {
-  const { openFilters } = useFilters();
-  const scale = useSharedValue(1);
-
-  const animatedStyle = useAnimatedStyle(() => {
-    return {
-      transform: [{ scale: scale.value }],
-    };
-  });
-
-  const handlePressIn = (ev: any) => {
-    scale.value = withTiming(0.95, {
-      duration: 100,
-    });
-    if (process.env.EXPO_OS === "ios") {
-      Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
-    }
-    props.onPressIn?.(ev);
-  };
-
-  const handlePressOut = (ev: any) => {
-    scale.value = withTiming(1, {
-      duration: 100,
-    });
-    props.onPressOut?.(ev);
-  };
-
-  return (
-    <AnimatedPressable
-      {...props}
-      style={[props.style, animatedStyle]}
-      onPressIn={handlePressIn}
-      onPressOut={handlePressOut}
-      onPress={() => {
-        openFilters();
-      }}
-    />
-  );
-}
 
 export default function TabLayout() {
   const { colors } = useTheme();
@@ -108,16 +56,6 @@ export default function TabLayout() {
               color={color}
             />
           ),
-        }}
-      />
-      <Tabs.Screen
-        name="filters"
-        options={{
-          title: "Filtry",
-          tabBarIcon: ({ color, focused }) => (
-            <SvgIcon Icon={FilterIconOutline} size={24} color={color} />
-          ),
-          tabBarButton: FiltersTabButton,
         }}
       />
       <Tabs.Screen
