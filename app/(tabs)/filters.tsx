6dc5b1cff0be006c42c8109dfad7e849df23530a
majasadowska <148617260+majasadowska@users.noreply.github.com>
2026-09-21 19:37:00 +0200

    Delete unused FiltersScreen component
    Remove unused FiltersScreen component as it is no longer needed.

--- app/(tabs)/filters.tsx
+++ app/(tabs)/filters.tsx
@@ -1,8 +1,1 @@
-import React from "react";
-import { View } from "react-native";
 
-// This screen is not used anymore since filters opens a bottom sheet
-// But we keep it to avoid routing errors
-export default function FiltersScreen() {
-  return <View></View>;
-}
